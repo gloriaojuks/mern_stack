83e8636a1f11852f78042803e73c1fc80db21c1c
Gloria Ojukwu <gloriaojukwu9@gmail.com>
2019-06-16 16:58:47 +0100

    first commit

---
+++ src/components/delete-todo.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+ class deleteTodo extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>delete me here</h1>
+            </div>
+        )
+    }
+}
+
+export default deleteTodo
